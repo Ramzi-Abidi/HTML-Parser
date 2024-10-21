67a50e06408f1b93c79f1ead4e1aaa091649f974
Ramzi-Abidi <ramziinfo2001@gmail.com>
2024-10-21 07:43:55 +0100

    feat/upload-html-file: Add file validation, file should be .html

--- src/routes/processHtmlRouter.ts
+++ src/routes/processHtmlRouter.ts
@@ -1,12 +1,17 @@
 import { Router } from "express";
 import { processHtmlInputController } from "../controllers/processHtmlController";
-import multer from 'multer';
+import { validateFile } from "../middlewares/validateFile";
+
+import multer from "multer";
 
 const r = Router();
-// Configure multer to store the file in memory
-const storage = multer.memoryStorage();
-const upload = multer({ storage });
 
-r.post("/extract", upload.single('htmlFile'), processHtmlInputController);
+const storage = multer.memoryStorage();
+const upload = multer({
+    storage,
+    fileFilter: validateFile,
+});
+
+r.post("/extract", upload.single("htmlFile"), processHtmlInputController);
 
 export default r;
