6ba70b21a742c6ea6afdcccad07531e6ef87cef8
Ramzi-Abidi <ramziinfo2001@gmail.com>
2024-10-21 07:27:34 +0100

    feat/upload-html-file: Add support for uploading HTML file using multer

--- src/routes/processHtmlRouter.ts
+++ src/routes/processHtmlRouter.ts
@@ -1,8 +1,12 @@
 import { Router } from "express";
 import { processHtmlInputController } from "../controllers/processHtmlController";
+import multer from 'multer';
 
 const r = Router();
+// Configure multer to store the file in memory
+const storage = multer.memoryStorage();
+const upload = multer({ storage });
 
-r.post("/extract", processHtmlInputController);
+r.post("/extract", upload.single('htmlFile'), processHtmlInputController);
 
 export default r;
