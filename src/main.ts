c14900c7cfb3783ffc8fe154b035d7852452f8fc
unknown <ramziabidi.dev@gmail.com>
2025-02-16 20:20:53 +0100

    Remove .env

--- src/main.ts
+++ src/main.ts
@@ -9,7 +9,7 @@ app.use(urlencoded({ extended: true }));
 
 app.use(processHtmlRouter);
 
-const port = process.env.PORT || 5000;
+const port = 5000;
 
 app.get("/", (req, res) => {
   res.status(200).json({
